206c34e5314b87bf5322ae5003b1287c3e888349
Henrik N <henrik@nyh.se>
2012-08-11 09:23:50 +0200

    Move Rack::Cache to config.ru.

--- app.rb
+++ app.rb
@@ -2,15 +2,6 @@ require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 
-# Defined in ENV on Heroku. To try locally, start memcached and uncomment:
-# ENV['MEMCACHE_SERVERS'] = "localhost"
-if memcache_servers = ENV['MEMCACHE_SERVERS']
-  use Rack::Cache,
-    verbose: true,
-    metastore:   "memcached://#{memcache_servers}",
-    entitystore: "memcached://#{memcache_servers}"
-end
-
 require "./scraper"
 require "./generator"
 
